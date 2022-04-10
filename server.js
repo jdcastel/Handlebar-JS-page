457e558a6d63b7180649632585ef5cd5d64a771c
Juan David <jdrodriguez-castelbl@myseneca.ca>
2022-04-09 21:47:35 -0500

    Assign 6

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 /*********************************************************************************
-*  WEB322 – Assignment 5
+*  WEB322 – Assignment 6
 *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
 *  No part of this assignment has been copied manually or electronically from any other source
 *  (including web sites) or distributed to other students.
@@ -25,13 +25,33 @@ const path = require("path");
 
 const blogService = require('./blog-service.js');
 const blogData = require("./blog-service");
+const authData = require("./auth-service.js");
 const { rmSync } = require("fs");
 const stripJs = require('strip-js');
+const clientSessions = require("client-sessions");
 //middleware
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", ".hbs");
+app.use(clientSessions( {
+    cookieName: "session",
+    secret: "web322_a6",
+    duration: 2 * 60 * 1000,
+    activeDuration: 1000 * 60
+}));
 
+app.use(function(req, res, next) {
+    res.locals.session = req.session;
+    next();
+});
+
+function ensureLogin(req, res, next) {
+    if (!req.session.user) {
+      res.redirect("/login");
+    } else {
+      next();
+    }
+}
 app.engine(".hbs", exphbs.engine({ 
     extname: ".hbs",
     helpers: {
@@ -241,22 +261,29 @@ app.post('/posts/add', upload.single("featureImage"), (req, res) => {
                         }
                     }
                 );
+
                 streamifier.createReadStream(req.file.buffer).pipe(stream);
             });
         };
+
         async function upload(req) {
             let result = await streamUpload(req);
             console.log(result);
             return result;
         }
+
         upload(req).then((uploaded) => {
             processPost(uploaded.url);
         });
     } else {
         processPost("");
     }
+
     function processPost(imageUrl) {
         req.body.featureImage = imageUrl;
+
+        // TODO: Process the req.body and add it as a new Blog Post before redirecting to /posts
+
         blogService.addPost(req.body).then(() => {
             res.redirect("/posts");
         }).catch(err => {
@@ -265,7 +292,7 @@ app.post('/posts/add', upload.single("featureImage"), (req, res) => {
     }
 });
 
-app.get('/post/:value', (req, res) => {
+app.get('/post/:value', ensureLogin, (req, res) => {
     blogService.getPostById(req.params.value).then((data) => {
         res.render("post", { post: data })
     }).catch(err => {
@@ -273,18 +300,12 @@ app.get('/post/:value', (req, res) => {
     });
 });
 
-// "Page Not Found" with an HTTP status code of 404.   
-app.use((req, res) => {
-    res.status(404);
-  });
-
-  app.get('/blog/:id', async (req, res) => {
+app.get('/blog/:id', ensureLogin, async (req, res) => {
 
     // Declare an object to store properties for the view
     let viewData = {};
 
     try {
-
         // declare empty array to hold "post" objects
         let posts = [];
 
@@ -329,9 +350,56 @@ app.use((req, res) => {
     res.render("blog", { data: viewData })
 });
 
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
+app.get("/register", (req, res) => {
+    res.render("register");
+});
+
+app.post("/register", (req,res) => {
+    authData.registerUser(req.body)
+    .then(() => res.render("register", {successMessage: "User created" } ))
+    .catch (err => res.render("register", {errorMessage: err, userName:req.body.userName }) )
+});
+
+app.post("/login", (req, res) => {
+    req.body.userAgent = req.get('User-Agent');
+    authData.checkUser(req.body).then((user) => {
+        req.session.user = {
+            userName: user.userName,
+            email: user.email,  
+            loginHistory: user.loginHistory 
+        }
+        res.redirect('/posts');
+    }).catch((err) => {
+        res.render("login", {errorMessage: err, userName: req.body.userName});
+    });
+});
+
+app.get("/logout", (req, res) => {
+    req.session.reset();
+    res.redirect("/");
+});
+
+app.get("/userHistory", ensureLogin, (req, res) => {
+    res.render("userHistory");
+});
+
+app.use((req, res) => {
+    res.status(404).send("Page Not Found");
+});
 
 // setup http server to listen on HTTP_PORT
 // app.listen(HTTP_PORT);
-blogService.initialize().then(
-    function(val){console.log("Loading...");app.listen(HTTP_PORT);}
-).catch(function(err){console.log(err)});
+blogService.initialize()
+    .then(authData.initialize)
+    .then(function () {
+        app.listen(HTTP_PORT, function () {
+            console.log("app listening on: " + HTTP_PORT)
+        });
+    }).catch(function (err) {
+        console.log("unable to start server: " + err);
+    }
+);
